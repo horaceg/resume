95721132fe1ec8ddcad7939f6a516f49b4d19629
Horace Guy <horace.guy.k@gmail.com>
2022-02-20 23:36:22 +0100

    use smarter split

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
   var elements = document.getElementsByTagName("details");
-  console.log(elements);
+//   console.log(elements);
   for (let e of elements) {
     if (window.innerWidth < 500) {
       e.open = false;
@@ -9,8 +9,8 @@ window.addEventListener("load", function () {
     }
     e.children[0].setAttribute(
       "preview",
-      e.outerHTML.split("</summary>")[1].slice(4, 20) + "..."
+      e.outerHTML.split("<p>")[1].slice(0, 23) + "..."
     );
-    console.log(e);
+    // console.log(e);
   }
 });
