76dbe27a8ac8dca623e2061d6521b5ede9d4106b
Horace Guy <horace.guy.k@gmail.com>
2022-02-23 14:02:02 +0100

    date in watch

--- build.fish
+++ build.fish
@@ -1,5 +1,7 @@
 #! /usr/bin/env fish
 
+date
+
 pandoc -s  --from markdown --to html -H (echo "<script>" (cat script.js) "</script>" | psub) -c style.css -o resume.html resume.md
 
 pandoc -s --from markdown --to html -V margin-top=0 -V margin-left=0 -V margin-right=0 -V margin-bottom=0 -V papersize=letter -c style.css -o horace_guy.pdf resume.md
