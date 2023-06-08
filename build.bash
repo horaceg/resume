36322bdd2e4e85b715535738420b70733ccb4606
Horace Guy <horace.guy.k@gmail.com>
2023-06-08 15:33:25 +0200

    enable local file access to wkhtmltopdf flag in pandoc call

--- build.bash
+++ build.bash
@@ -5,10 +5,12 @@ set -e
 date
 
 pandoc -s --from markdown --to html \
+    --pdf-engine-opt=--enable-local-file-access \
     -H <(echo "<script>" $(cat script.js) "</script>") \
     -c style.css -o index.html resume.md
 
 pandoc -s --from markdown --to html \
+    --pdf-engine-opt=--enable-local-file-access  \
     -V margin-top=0 -V margin-left=0 -V margin-right=0 -V margin-bottom=0 \
     -V papersize=letter \
     -c style.css -o horace_guy.pdf <(head -n -4 resume.md)
