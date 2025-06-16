a24113f1a47261682e1099c05440a6b8089ebfe7
Horace Guy <horace.guy.k@gmail.com>
2025-06-16 20:18:54 +0200

    cwd

--- build.bash
+++ build.bash
@@ -8,7 +8,8 @@ pandoc -s --from markdown --to html \
     -H <(echo "<script>" $(cat script.js) "</script>") \
     -c style.css -o index.html resume.md
 
-node print.js ./index.html rendered/horace_guy.pdf
+cwd=$(pwd)
+node print.js $cwd/index.html rendered/horace_guy.pdf
 
 mkdir -p public
 cp *.{html,css} public/
