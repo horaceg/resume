d833a37d6920b9752e178a36a4529f5d68664e26
Horace Guy <horace.guy.k@gmail.com>
2025-06-16 20:25:45 +0200

    fix build

--- build.bash
+++ build.bash
@@ -9,8 +9,8 @@ pandoc -s --from markdown --to html \
     -c style.css -o index.html resume.md
 
 cwd=$(pwd)
-node print.js $cwd/index.html rendered/horace_guy.pdf
+node print.js $cwd/index.html horace_guy.pdf
 
 mkdir -p public
 cp *.{html,css} public/
-cp rendered/*.pdf public/
+cp horace_guy.pdf public/
